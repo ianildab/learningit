70322ab041acaa35fad6cb4f8581cbc2bb854bdc
ianildab <ianildab@gmail.com>
2015-12-13 20:44:12 +0530

    Update addschedule.java

--- addschedule.java
+++ addschedule.java
@@ -11,7 +11,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.servlet.ServletException;
+//import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
